f9e798edde649b1ef12ddedd2607b9e67d7ea8a1
rlopez <ronald.lopez@exte.com>
2025-04-02 20:06:11 -0500

    feat: remove int en el stteings

--- Gestion_Backend/settings.py
+++ Gestion_Backend/settings.py
@@ -189,7 +189,7 @@ CORS_ALLOW_HEADERS = [
 # Configuración de MailHog para Django
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 1025))  # Asegurar que sea un número
+EMAIL_PORT = os.getenv('EMAIL_PORT', 1025)  # Asegurar que sea un número
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'False') == 'True'  # Convertir a booleano
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'  # Convertir a booleano
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
